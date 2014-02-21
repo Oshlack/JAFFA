6c6600ce86ada169d05c8c1de44d56ed9862ca84
nadiamdavidson@gmail.com <nadiamdavidson@gmail.com@83c0ce6c-5703-8ab6-7442-2afde5b00016>
2014-02-21 00:16:40 +0000

    pipeline for using reads directly

---
+++ JAFFA_no_assemble.groovy
@@ -0,0 +1,40 @@
+/***********************************************************
+ ** This is the JAFFA pipeline file for fusion detection
+ ** without read assembly. Run like so:
+ **    bpipe run <path_to_this_file> <path_to_fastq/fasta_files>
+ ** See our website for details	on running options:
+ ** https://code.google.com/p/jaffa-project/.
+ **
+ ** Author: Nadia Davidson <nadia.davidson@mcri.edu.au>
+ ** Last Update: 6th Feb 2014
+ *********************************************************/
+
+commands="trimmomatic R fastx_collapser bowtie2 blat fasta_formatter"
+load "JAFFA_stages.groovy"
+
+body = segment { align_transcripts_to_annotation +
+                 filter_transcripts +
+                 extract_fusion_sequences +
+                 align_transcripts_to_genome +
+                 make_simple_reads_table +
+                 get_final_list }
+
+get_unmapped_as_fasta = segment { prepare_reads + cat_reads + remove_dup + get_unmapped }
+
+// below is the pipeline for a fasta file
+if(args[0].endsWith(fasta_suffix)) {
+   run { run_check + fasta_input_format * [
+	     make_dir_using_fasta_name + 
+	     body ] + compile_all_results
+   } 
+// or you can provide the reads and they will be 
+// filtered and converted to fasta before running
+// the same pipeline as above
+} else {
+   run { run_check + fastq_input_format * [
+       	    make_dir_using_fastq_names +
+	    get_unmapped_as_fasta +
+	    body ] + compile_all_results 
+   }
+}
+
