c7100f42039630d4d3e0a7a30d31e62c477a25d4
nadiamdavidson@gmail.com <nadiamdavidson@gmail.com@83c0ce6c-5703-8ab6-7442-2afde5b00016>
2014-11-06 06:01:54 +0000

    Fixing up the fasta version of direct mode

--- JAFFA_direct.groovy
+++ JAFFA_direct.groovy
@@ -9,18 +9,12 @@
  ** Last Update: 6th Feb 2014
  *********************************************************/
 
-//commands="trimmomatic R fastx_collapser bowtie2 blat fasta_formatter"
-
 codeBase = file(bpipe.Config.config.script).parentFile.absolutePath
 load codeBase+"/JAFFA_stages.groovy"
 
-body = segment { filter_transcripts +
+common_steps = segment { filter_transcripts +
                  extract_fusion_sequences +
-                 align_transcripts_to_genome +
-                 make_simple_reads_table +
-                 get_final_list }
-
-//get_unmapped_as_fasta = segment { prepare_reads + cat_reads + remove_dup + get_unmapped }
+                 align_transcripts_to_genome }
 
 get_unmapped_as_fasta = segment { prepare_reads + get_unmapped }
 
@@ -30,7 +24,9 @@ if(args[0].endsWith(fastaSuffix)) {
    run { run_check + fastaInputFormat * [
 	     make_dir_using_fasta_name + 
 	     align_transcripts_to_annotation +
-	     body ] + compile_all_results
+	     common_steps + 
+	     make_fasta_reads_table +
+	     get_final_list ] + compile_all_results
    } 
 // or you can provide the reads and they will be 
 // filtered and converted to fasta before running
@@ -41,7 +37,9 @@ if(args[0].endsWith(fastaSuffix)) {
       	    make_dir_using_fastq_names +
 	    get_unmapped_as_fasta +
 	    align_reads_to_annotation +
-	    body ] + compile_all_results 
+	    common_steps +
+	    make_simple_reads_table +
+	    get_final_list ] + compile_all_results 
    }
 }
 
