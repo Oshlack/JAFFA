8ccd57d2016974c9a9a068be372453a79b2b9772
Rebecca Evans <rebecca.evans@mcri.edu.au>
2016-12-27 12:44:57 +1100

    Added Docker files for running JAFFA within Docker.  See also https://hub.docker.com/r/beccyl/jaffa

---
+++ docker/tools.groovy
@@ -0,0 +1,12 @@
+// Path to tools used by the JAFFA pipeline
+bpipe="bpipe"
+velveth="velveth"
+velvetg="velvetg"
+oases="oases"
+trimmomatic="java -jar /usr/share/java/trimmomatic.jar \$@"
+samtools="samtools"
+bowtie2="bowtie2"
+blat="blat"
+dedupe="/opt/bbmap/dedupe.sh \$@"
+reformat="/opt/bbmap/reformat.sh \$@"
+R="R"
