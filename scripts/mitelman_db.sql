98ed692e83c1aa825df12371f7fd9c37a6f5905c
Shiyi Yin <38135561+yinshiyi@users.noreply.github.com>
2026-02-17 02:35:20 +0000

    fix grammar

--- scripts/mitelman_db.sql
+++ scripts/mitelman_db.sql
@@ -1,6 +1,6 @@
-# This is the bigquery script to programtic download mitelman database fusion events
-# run with google account and update dates accordingly, 4 releases per year
-# check `isb-cgc-bq.mitelman_versioned.INFORMATION_SCHEMA.TABLES`
+# This is a bigquery script to download fusion events from mitelman database
+# Run with google account and update dates accordingly, 4 releases per year
+# Check `isb-cgc-bq.mitelman_versioned.INFORMATION_SCHEMA.TABLES`
 SELECT g.Gene, COUNT(*) AS CNT
 FROM `isb-cgc-bq.mitelman_versioned.MolBiolClinAssoc_2026_01` c
 JOIN `isb-cgc-bq.mitelman_versioned.MolClinGene_2026_01` g
