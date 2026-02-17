47514d0d5124af2632fba90b1eee0998ea4d67fe
Shiyi Yin <38135561+yinshiyi@users.noreply.github.com>
2026-02-17 02:24:59 +0000

    output the gene-fusion count table

--- scripts/mitelman_db.sql
+++ scripts/mitelman_db.sql
@@ -1,13 +1,14 @@
 # This is the bigquery script to programtic download mitelman database fusion events
 # run with google account and update dates accordingly, 4 releases per year
 # check `isb-cgc-bq.mitelman_versioned.INFORMATION_SCHEMA.TABLES`
-  with a1 as (SELECT g.Gene, COUNT(*) AS CNT
-    FROM `isb-cgc-bq.mitelman_versioned.MolBiolClinAssoc_2026_01` c
-    JOIN `isb-cgc-bq.mitelman_versioned.MolClinGene_2026_01` g
-    ON (g.RefNo = c.RefNo AND g.InvNo = c.InvNo AND g.MolClin = c.MolClin)
-    WHERE g.Gene LIKE '%::%'
-    AND g.MolClin = 'M'
-    AND c.GeneShort NOT LIKE '%::%,%::%'
-    GROUP BY Gene
-    ORDER BY CNT DESC, Gene)
-    Select SUM(CNT) from a1
+SELECT g.Gene, COUNT(*) AS CNT
+FROM `isb-cgc-bq.mitelman_versioned.MolBiolClinAssoc_2026_01` c
+JOIN `isb-cgc-bq.mitelman_versioned.MolClinGene_2026_01` g
+  ON (g.RefNo = c.RefNo 
+      AND g.InvNo = c.InvNo 
+      AND g.MolClin = c.MolClin)
+WHERE g.Gene LIKE '%::%'
+  AND g.MolClin = 'M'
+  AND c.GeneShort NOT LIKE '%::%,%::%'
+GROUP BY Gene
+ORDER BY CNT DESC, Gene
